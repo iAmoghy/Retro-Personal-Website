d2a98683cfcf644254929f85eb662b4f3a4f81ec
iAmoghy <70843557+iAmoghy@users.noreply.github.com>
2022-08-12 14:39:41 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function dark_mode_toggle() {
+    document.body.style.backgroundColor= 'black';
+    document.body.style.color = 'white';
+}
+
+function light_mode_toggle() {
+    document.body.style.backgroundColor = 'white';
+    document.body.style.color = 'black';
+
+}
